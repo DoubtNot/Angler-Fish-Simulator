5595e00b414b12110341a030188af4aebc8a231f
Spencer Anderson <lastof6.boyz@gmail.com>
2023-06-12 00:22:05 -0600

    Camera Joystick and Rain Particle System. Volcano Smoke Mesh  Changed.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+
+public class CameraController : MonoBehaviour
+{
+    public Joystick joystick;
+    public CinemachineFreeLook freeLookCamera;
+    public float rotationSpeed = 1.5f;
+    public float verticalSpeedMultiplier = 0.5f;
+
+    void Update()
+    {
+        // Get joystick input values
+        float horizontalInput = joystick.Horizontal;
+        float verticalInput = joystick.Vertical;
+
+        // Calculate rotation angles based on joystick input
+        float rotationX = verticalInput * rotationSpeed * verticalSpeedMultiplier;
+        float rotationY = horizontalInput * rotationSpeed;
+
+        // Adjust the camera's rotation using Cinemachine
+        freeLookCamera.m_XAxis.Value += rotationY;
+        freeLookCamera.m_YAxis.Value += rotationX;
+    }
+}
